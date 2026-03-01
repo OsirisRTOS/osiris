fa054461d2737e3f208de111563f510af3f58a69
Philipp Erhardt <philipp.erhardt@tum.de>
2026-03-01 11:49:49 +0000

    STM32L4: support UART5 output

--- machine/arm/stm32l4xx/interface/uart.c
+++ machine/arm/stm32l4xx/interface/uart.c
@@ -1,13 +1,13 @@
 
 #include "lib.h"
+#include "stm32l4r5xx.h"
+#include "stm32l4xx_hal_gpio.h"
+#include "stm32l4xx_hal_rcc.h"
+#include "stm32l4xx_hal_rcc_ex.h"
 
 #include <stm32l4xx_hal.h>
 
 static UART_HandleTypeDef HDBG_UART;
-
-#ifndef OSIRIS_DEBUG_UART
-  #error "OSIRIS_DEBUG_UART not defined."
-#endif
 
 int init_debug_uart(void) {
   HDBG_UART.Instance = OSIRIS_DEBUG_UART;
@@ -50,5 +50,31 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart) {
     HAL_PWREx_EnableVddIO2();
     __HAL_RCC_GPIOG_CLK_ENABLE();
     HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+  } else if (huart->Instance == UART5) {
+
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_UART5;
+    PeriphClkInit.Uart5ClockSelection = RCC_UART5CLKSOURCE_PCLK1;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+      return;
+    }
+
+    __HAL_RCC_UART5_CLK_ENABLE();
+
+    // Enable GPIO clocks
+    HAL_PWREx_EnableVddIO2();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_12; // PC12 == UART5_TX
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF8_UART5;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    // PD2 == UART5_RX
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
   }
 }
