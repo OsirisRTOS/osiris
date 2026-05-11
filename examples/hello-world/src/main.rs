97cfbd99e8226066cbab2ff1c30d92a1aa65a3d1
thomasw04 <35061939+thomasw04@users.noreply.github.com>
2026-05-11 08:29:50 +0000

    make print implementable from uspace

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -3,12 +3,30 @@
 
 use osiris::app_main;
 
+macro_rules! println {
+    ($($arg:tt)*) => {{
+        use osiris::uapi::*;
+        // Print seconds and microseconds since boot.
+        let (secs, frac) = to_secs(time::mono_now(), time::mono_freq() as u32, 6);
+        print::print(format_args!("[{}.{:06}] ", secs, frac));
+        print::print(format_args!($($arg)*));
+        print::print(format_args!("\n"));
+    }};
+}
+
+pub fn to_secs(cnt: u64, hz: u32, digits: u8) -> (u64, u64) {
+    let secs = cnt / (hz as u64);
+    let rem = cnt % (hz as u64);
+    let frac = (rem * 10_u64.pow(digits as u32)) / (hz as u64);
+    (secs, frac)
+}
+
 extern "C" fn second_thread() {
     let mut time = osiris::uapi::time::tick();
     let mut cnt = 0;
     loop {
         time += 100;
-        osiris::uprintln!("Number: {}", cnt);
+        println!("Number: {}", cnt);
         cnt += 1;
         osiris::uapi::sched::sleep(time);
     }
@@ -16,7 +34,7 @@ extern "C" fn second_thread() {
 
 #[app_main]
 fn main() {
-    osiris::uprintln!("Hello World!");
+    println!("Hello World!");
     let mut tick = 0;
     let attrs = osiris::uapi::sched::RtAttrs {
         deadline: 100,
@@ -26,7 +44,7 @@ fn main() {
 
     osiris::uapi::sched::spawn_thread(second_thread, Some(attrs));
     loop {
-        osiris::uprintln!("Tick: {}", tick);
+        println!("Tick: {}", tick);
         tick += 1;
         osiris::uapi::sched::sleep_for(1000);
     }
